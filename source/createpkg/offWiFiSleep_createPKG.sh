a4094cc456a08817ae9344183c25c54ce26b053f
TonyLiu2ca <tonyliu2ca@yahoo.com>
2017-11-01 21:22:30 -0600

    1.1.1
    Fix

--- source/createpkg/offWiFiSleep_createPKG.sh
+++ source/createpkg/offWiFiSleep_createPKG.sh
@@ -10,24 +10,20 @@ identifier="com.github.TonyLiu2ca"
 version="1.1.1"
 
 homePath=$(dirname "$0")
-<<<<<<< HEAD
 pushd "$homePath/.." &>/dev/null
-=======
-pushd "$homePath/.." 1>/dev/null 2>1
->>>>>>> 3f27f239d75391d27732c097def9bdd2a4a261d4
 if [ "$EUID" -ne 0 ];then echo "Please run as root"; exit; fi
 
 # Delete extra file.
 find . -name '.DS_Store' -type f -delete &>/dev/null
 
 # Correct files' permission.
-chflags -R noschg "./root/Library/Application Support/$package"
-chflags -R nouchg "./root/Library/Application Support/$package"
+chflags -R noschg "./root/Library/$package"
+chflags -R nouchg "./root/Library/$package"
 /usr/bin/xattr -c -r "./root/"
-chmod -RN "./root/Library/Application Support/$package"
-chown -R root:wheel "./root/Library/Application Support/$package"
-chmod -R 755  "./root/Library/Application Support/$package"
-chmod 777 "./root/Library/Application Support/$package/${package}_Settings.ini"
+chmod -RN "./root/Library/$package"
+chown -R root:wheel "./root/Library/$package"
+chmod -R 755  "./root/Library/$package"
+chmod 777 "./root/Library/$package/${package}_Settings.ini"
 chmod 644 "./root/Library/LaunchDaemons/${identifier}.${package}.plist"
 
 # Create the installation package.
@@ -36,3 +32,5 @@ pkgbuild --root root --scripts scripts --identifier "${identifier}.${package}" -
 chmod 777 "${package}.pkg"; chown `id -un` "${package}.pkg"; mv -f "${package}.pkg" build
 # done
 popd &>/dev/null
+
+ 
